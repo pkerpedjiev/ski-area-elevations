fad57e165a569b3631377f29292afb07332e9382
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2016-02-07 22:34:28 +0100

    Continuing with the make_tiles function

--- scripts/make_tiles.py
+++ scripts/make_tiles.py
@@ -4,6 +4,41 @@ import json
 import sys
 from optparse import OptionParser
 
+def make_tiles(entries, options, start_x):
+    """
+    Create tiles for all of the passed in entries.
+
+    If the passed in entries have the following positions and areas:
+
+    4 15.99
+    1 7.27
+    6 3.88
+    7 3.05
+    5 3.02
+    2 2.99
+    3 2.48
+
+    And we only allow one area per tile, then the returned
+    tiles will be:
+
+    {current: [4], from: 1, to: 7,
+        left: { current: [1], from: 1, to: 3,
+           left: { from: 1, to: 1, current: [1] }
+           right: { from: 2, to: 3, current: [2] 
+            left
+        }
+        right:  { current: [4], from: 4, to: 7,
+
+        }
+    }
+
+    :entries: The list of objects to make tiles for
+    :options: Options passed in to the program
+    :start_x: The initial x position
+    :returns:
+    """
+    pass
+
 def main():
     usage = """
     python make_tiles.py json_file
@@ -33,7 +68,6 @@ def main():
         entries = sorted(entries, key= lambda x: -float(x[options.importance]))
 
         print >>sys.stderr, "Entries:", entries
-        
 
 if __name__ == '__main__':
     main()
