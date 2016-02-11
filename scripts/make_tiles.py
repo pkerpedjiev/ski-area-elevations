95093082ebedd889247c3504eb12eccc730031b9
Peter Kerpedjiev <pkerp@tbi.univie.ac.at>
2016-02-11 23:13:10 +0100

    A little work on the tile creation

--- scripts/make_tiles.py
+++ scripts/make_tiles.py
@@ -4,39 +4,71 @@ import json
 import sys
 from optparse import OptionParser
 
-def make_tiles(entries, options, start_x):
+def make_tiles(entries, options, zoom_level, start_x, end_x):
     """
     Create tiles for all of the passed in entries.
 
     If the passed in entries have the following positions and areas:
 
-    4 15.99
-    1 7.27
-    6 3.88
-    7 3.05
-    5 3.02
-    2 2.99
-    3 2.48
+    3 15.99
+    0 7.27
+    5 3.88
+    6 3.05
+    4 3.02
+    1 2.99
+    2 2.48
+
+    0 1 2 3 4 5 6
 
     And we only allow one area per tile, then the returned
     tiles will be:
 
-    {current: [4], from: 1, to: 7,
-        left: { current: [1], from: 1, to: 3,
-           left: { from: 1, to: 1, current: [1] }
-           right: { from: 2, to: 3, current: [2] 
-            left
+    Left: always from start(parent) to start(parent) + (end(parent) - start(parent) / 2)
+    right: always from (end(parent) - start(parent) / 2) + 1 to end(parent)
+    shown: the ones with the maximum values within that tile
+
+
+    {shown: [3], from: 0, to: 6, zoom: 0,
+        left: { shown: [3], from: 0, to: 3, zoom: 1
+           left: { from: 0, to: 1, shown: [0], zoom: 1
+              left: {from: 0, to: 0, shown: [0] }
+              right: {from: 1, to 1, shown: [1] }
+           }
+           right: { from: 2, to: 3, shown: [2] 
+              left { from: 2, to: 2, shown: [2] },
+              right { from 3, to: 3, shown: [3] }
+           }
         }
-        right:  { current: [4], from: 4, to: 7,
+        right:  { shown: [4], from: 4, to: 7,
 
         }
     }
 
     :entries: The list of objects to make tiles for
     :options: Options passed in to the program
+    :zoom_level: The current zoom level
     :start_x: The initial x position
+    :end_x: The final x position
     :returns:
     """
+    # show only a subset of the entries that fall within this tile
+    tile = {}
+    tile['shown'] = sorted(entries, key=lambda x: x[options.importance])[:options.max_entries_per_tile]
+    tile['start_x'] = start_x
+    tile['end_x'] = end_x
+    tile['zoom'] = zoom_level
+
+    midpoint = (end_x - start_x) / 2
+
+    left_entries = filter(lambda x: x[options.position] <= midpoint, entries)
+    right_entries = filter(lambda x: x[options.position] > midpoint, entries)
+
+    if zoom < options.max_zoom:
+        tiles += make_tiles(left_entries, options, zoom_level+1, start_x = start_x,
+                end_x = midpoint)
+        tiles += make_tiles(right_entries, options, zoom_level+1, start_x = midpoint,
+                end_x = end_x)
+
     pass
 
 def main():
@@ -56,6 +88,10 @@ def main():
     parser.add_option('-p', '--position', dest='position', default='position',
             help='Where this entry would be placed on the x axis',
             type='string')
+    parser.add_option('-e', '--max-entries-per-tile', dest='max_entries_per_tile', default=100,
+        help='The maximum number of entries that can be displayed on a single tile')
+    parser.add_option('-m', '--max-zoom', dest='max_zoom', default=5,
+            help='The maximum zoom level')
 
     (options, args) = parser.parse_args()
 
@@ -65,9 +101,17 @@ def main():
 
     with open(args[0], 'r') as f:
         entries = json.load(f)
+
+        options.max_pos = max(map(lambda x: x[options.position], entries))
+        options.min_pos = min(map(lambda x: x[options.position], entries))
+
         entries = sorted(entries, key= lambda x: -float(x[options.importance]))
 
+        make_tiles(entries, options, zoom_level = 0, 
+                   start_x = options.min_pos, end_x = options.max_pos)
+
         print >>sys.stderr, "Entries:", entries
+
 
 if __name__ == '__main__':
     main()
